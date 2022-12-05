8e5cec283a6d044c1aa99083742d40a8a7562e4f
TachiMeow <92790926+TachiMeow@users.noreply.github.com>
2022-12-05 22:39:04 +0300

    Add files via upload

--- src/parsers/linkedIn_parser/link.py
+++ src/parsers/linkedIn_parser/link.py
@@ -1,75 +1,90 @@
 import random
 import time
 import json
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+import re
+import fake_useragent
+import requests
+from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 class Parser:
     def __init__(self):
         self.start = 0
         self.step = 25
+        self.html_elements = []
+        self.links = []
+        self.main_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?trk=guest_homepage-basic_guest_nav_menu_jobs&start='
 
-    def get_data(self, url):
-        html_elements = []
-        for i in range(0, 39):
-            try:
-                links = []
-                try:
-                    driver.get(url + str(self.start))
-                except Exception as error:
-                    print('URL error')
-                    print(error)
+    def generate_url(self, url):
+        return url + str(self.start)
 
-                elements = driver.find_elements(By.TAG_NAME, 'a')
-                if len(elements) == 0:
-                    break
+    def set_links_from_one_page(self, url):
+        try:
+            user = fake_useragent.UserAgent()
+            data = requests.get(
+                url=url,
+                headers={'user-agent': user.random}
+            )
+            if data.status_code == 200:
+                soup = BeautifulSoup(data.content, 'lxml')
+                for a in soup.find_all('a', {'class': 'base-card__full-link'}):
+                    self.links.append(a.attrs['href'])
+            elif data.status_code == 429:
+                print(data.status_code)
+                time.sleep(random.randint(30, 60))
+                self.set_links_from_one_page(url)
+            else:
+                print(data.status_code)
+                time.sleep(random.randint(30, 60))
+                self.set_links_from_one_page(url)
+        except Exception as error:
+            print(error, ' set')
 
-                try:
-                    for element in elements:
-                        href = element.get_attribute('href')
-                        if href[0:35] == 'https://www.linkedin.com/jobs/view/':
-                            links.append(href)
-                except Exception as error:
-                    print('Cannot find @href')
-                    print(error)
+    def get_data_from_link(self, url):
+        try:
+            user = fake_useragent.UserAgent()
+            data = requests.get(
+                url=url,
+                headers={'user-agent': user.random}
+            )
 
-                for link in links:
-                    driver.get(link)
-                    desc = []
-                    try:
-                        description = driver.find_elements(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/section[1]/div/div/section/div')
-                        for d in description:
-                            if d != []:
-                                desc.append(d.text)
-                        html_elements.append(
-                            {
-                            '1': driver.find_elements(By.XPATH, '//*[@id="main-content"]/section[1]/div/section[2]/div/div[1]/div/h1')[0].text,
-                            '2': driver.find_elements(By.XPATH, '//*[@id="main-content"]/section[1]/div/section[2]/div/div[1]/div/h4/div[1]/span[1]/a')[0].text,
-                            '3': driver.find_elements(By.XPATH, '//*[@id="main-content"]/section[1]/div/section[2]/div/div[1]/div/h4/div[1]/span[2]')[0].text,
-                            '4': driver.find_elements(By.XPATH, '//*[@id="main-content"]/section[1]/div/section[2]/div/div[1]/div/h4/div[2]/span[1]')[0].text,
-                            '5': driver.find_elements(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/section[1]/div/ul/li[1]')[0].text,
-                            '6': desc
-                            }
-                        )
-                    except Exception as error:
-                        print(error)
-                self.start += self.step
-                time.sleep(random.randint(2, 4))
-            except Exception as error:
-                print(error)
+            if data.status_code == 200:
+                soup = BeautifulSoup(data.content, 'lxml')
+                self.html_elements.append(
+                    {
+                        'Job Name': re.sub(r'\s+', ' ', soup.find('h1', {'class': 'top-card-layout__title'}).text.replace('\n', '')),
+                        'Org. Name': re.sub(r'\s+', ' ', soup.find('a', {'class': 'topcard__org-name-link'}).text.replace('\n', '')),
+                        'Org. City': re.sub(r'\s+', ' ', soup.find('span', {'class': 'topcard__flavor--bullet'}).text.replace('\n', '')),
+                        'Posted Time': re.sub(r'\s+', ' ', soup.find('span', {'class': 'posted-time-ago__text'}).text.replace('\n', '')),
+                        'List of Job Criteris': [re.sub(r'\s+', ' ',criteris.text.replace('\n', ' ')) for criteris in soup.find_all('span', {'class', 'description__job-criteria-text--criteria'})],
+                        'Full Description': soup.find('div', {'class', 'show-more-less-html__markup'}).text.replace('\n', ' ')
+                    }
+                )
+                #print(self.html_elements[-1])
+            elif data.status_code == 429:
+                print(data.status_code)
+                time.sleep(random.randint(30, 60))
+                self.get_data_from_link(url)
+            else:
+                print(data.status_code)
+                time.sleep(random.randint(30, 60))
+                self.get_data_from_link(url)
 
-        return html_elements
+        except Exception as error:
+            print(error, ' get')
 
 def main():
-    url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?trk=guest_homepage-basic_guest_nav_menu_jobs&start='
-    P = Parser()
-    data = P.get_data(url)
+    p = Parser()
+    for i in range(0, 1): # 0 , 25 , 50, 75 -> 1000:max => (0, 39)
+        p.set_links_from_one_page(url=p.generate_url(p.main_url))
+        p.start += p.step
+        time.sleep(random.randint(30, 60))
+    for l in p.links:
+        p.get_data_from_link(l)
+        time.sleep(random.randint(3, 15))
+
     with open("data.json", "a", encoding="utf-8") as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
+        json.dump(p.html_elements, file, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
     main()
