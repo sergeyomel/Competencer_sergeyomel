1291c39f7e6c94313486bd7f7d7a20118b5a698e
andD00m <korsun3@bk.ru>
2022-12-18 23:02:24 +0300

    Исправлена загрузка JSON

--- src/parsers/hh_parser/get_hh.py
+++ src/parsers/hh_parser/get_hh.py
@@ -5,7 +5,9 @@ from src.db.requests.DbLoader import DbLoader
 
 def get_hh():
     loader = DbLoader()
+    vacancies = []
     for a in get_links():
-        data = json.dumps(get_vacancies(a))
-
-        loader.load(data)
+        vacancies.append(get_vacancies(a))
+    with open('data.json', 'a', encoding='utf-8') as f:
+        json.dump(vacancies, f, indent=4)
+    loader.load(vacancies)
