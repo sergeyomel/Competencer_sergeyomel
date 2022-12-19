13a0c5d8e0dedcebd1109e1f300c8323bfa047d6
andD00m <korsun3@bk.ru>
2022-12-20 00:41:55 +0300

    Откорректирована запись в JSON файл

--- src/parsers/hh_parser/get_hh.py
+++ src/parsers/hh_parser/get_hh.py
@@ -1,15 +1,30 @@
 import json
+import os
+
 from hh_subfun import get_links, get_vacancies
 from src.db.requests.DbLoader import DbLoader
 
 
 def get_hh():
     loader = DbLoader()
-    vacancies = []
-    for a in get_links():
-        vacancies.append(get_vacancies(a))
     with open('data.json', 'a', encoding='utf-8') as f:
-        json.dump(vacancies, f, indent=4)
-    loader.load(vacancies)
+        f.write('[')
+    try:
+        for a in get_links():
+            data = get_vacancies(a)
+            with open('data.json', 'a', encoding='utf-8') as f:
+                json.dump(data, f, indent=4)
+                f.write(',\n')
+    except:
+        pass
+    finally:
+        with open('data.json', 'rb+', encoding='utf-8') as f:
+            f.seek(-1, os.SEEK_END)
+            f.truncate()
+            f.write(']')
+    with open('data.json', 'r', encoding='utf-8') as f:
+        data = f.read()
+        loader.load(data)
+
 
 get_hh()
