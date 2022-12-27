7227b44bc125937bebdfc658ebbcd2ce79d71ac5
sergeyomel <sergeyomel1work@gmail.com>
2022-12-27 23:55:24 +0300

    Добавлено приведение содержимого файла к нужному формату

--- src/parsers/hh_parser/HhLoader.py
+++ src/parsers/hh_parser/HhLoader.py
@@ -25,6 +25,7 @@ class HhLoader():
             with open(json_file_path, encoding='utf-8', mode='r') as f:
                 txt_data = f.read()
                 txt_data = txt_data.replace('\xa0', '').replace('&quot', '')
+            txt_data = '[' + txt_data[:-2] + ']'
 
             json_data = json.loads(txt_data)
 
